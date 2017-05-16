8a5f4b57a35d6fbdc5905d5108b584a389572b6b
Jefferson Viana Fonseca Abreu <jeffvfa@hotmail.com>
2017-05-16 18:52:50 +0000

    começo de implementação, com a criação da thread aviao

---
+++ main.c
@@ -0,0 +1,60 @@
+/*
+* @author: @jeffvfa github.com/jeffvfa
+*/
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <pthread.h>  
+#include <semaphore.h> 
+
+#define NUMAVI 100 
+
+//pista de pouso 
+sem_t pista; 
+
+//thread avião 
+pthread_t avioes[NUMAVI]; 
+
+/* 
+*informação se o avião está voando
+*   se voando igual a 1, o avião está voando, 
+*   se igual a 0 está pousado;
+*/
+int se_voando[NUMAVI];
+
+
+//declarações de funções 
+void* aviao();
+
+
+
+//implementação de funções
+//avião 
+void* aviao(void* args){ 
+    int id = *((int*) args); 
+    
+    while(1){
+        printf("\t %d \n",id);
+        //TODO
+    }
+}
+
+
+int main(){
+    //declarações de variáveis
+    int i =0, *id;
+    
+    //inicialização da pista
+    sem_init(&pista,0,1);
+    
+    for(i=0;i<NUMAVI;i++){ 
+        id = (int *) malloc(sizeof(int));
+        *id = i;
+        pthread_create(&(avioes[i]), NULL, aviao,id);
+    }
+    
+    printf("Olá");
+    
+    getchar(); 
+    return 0;
+} 
